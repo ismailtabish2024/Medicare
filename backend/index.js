fe937f1eff75395f226baf4bc7328dde40254f9f
Ismail Tabish <ismailtabish17@gmail.com>
2026-05-04 18:36:42 +0530

    port error fixed

--- backend/index.js
+++ backend/index.js
@@ -55,8 +55,8 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening on port http://localhost:${port}`);
+// });
 
 
