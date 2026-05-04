63ebc41826d31c13a4f42a2a718473ed77f8801d
Ismail Tabish <ismailtabish17@gmail.com>
2026-05-04 21:36:21 +0530

    url

--- backend/index.js
+++ backend/index.js
@@ -19,8 +19,8 @@ app.listen(PORT, () => {
 const allowedOrigins = [
   // 'http://localhost:5173',
   // 'http://localhost:5174',
-    'https://medicare-taupe-ten.vercel.app',
-  'https://medicare-9qea.vercel.app'
+    'https://medicare-9qea.vercel.app/h', // Admin 
+  'https://medicare-frontend-zeta-ten.vercel.app/' // frontend
 ];
 
 app.use(cors({
