7ae9c6422c9ab3b6fe417692c71a2fc66bd70049
Ismail Tabish <ismailtabish17@gmail.com>
2026-05-04 19:25:01 +0530

    frontend url fixed

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.listen(PORT, () => {
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
-  'https://your-frontend.vercel.app',
+    'https://medicare-taupe-ten.vercel.app/',
   'https://your-admin.vercel.app'
 ];
 
